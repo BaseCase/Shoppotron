2542eb44ed2a548e722a0a05bd7faa472bce14e9
Casey Brant <casey@caseybrant.com>
2017-12-02 15:41:54 -0600

    Basic pause menu

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour 
+{
+    public GameObject pause_menu;
+
+    private bool paused;
+    private float time_scale;
+
+	void Start () 
+    {
+        paused = false;
+        time_scale = Time.timeScale;
+
+	}
+	
+	void Update () 
+    {
+        if (Input.GetKeyDown("escape")) {
+            if (paused) {
+                paused = false;
+                Time.timeScale = time_scale;
+            } else {
+                paused = true;
+                Time.timeScale = 0f;
+            }
+            pause_menu.SetActive(paused);
+        }
+	}
+}
