01bf65e997dddca02beec03c0b7a3b8dfb6140ec
Casey Brant <casey@caseybrant.com>
2017-12-02 10:50:24 -0600

    Pickups with shell of shopping cart

---
+++ Assets/Scripts/PickupController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour 
+{
+    public GameObject shopping_cart;
+
+    private ShoppingCartController cart;
+
+	void Start () 
+    {
+        cart = shopping_cart.GetComponent<ShoppingCartController>();
+	}
+	
+    void Update () 
+    {
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        cart.add_item(gameObject);
+
+        // TODO: a little animation or something before destroying
+        // TODO: Destroy is probably not the right thing; we want to keep it around so
+        //       that the shopping cart can use it.
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ShoppingCartController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingCartController : MonoBehaviour 
+{
+	void Start () 
+    {
+		
+	}
+
+    void Update () 
+    {
+		
+	}
+
+    public void add_item(GameObject item)
+    {
+        Debug.Log("OK we are adding it" + item.ToString());
+    }
+}
