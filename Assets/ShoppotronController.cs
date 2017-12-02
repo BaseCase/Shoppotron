7b4a2b527ad6c3f927cbda50ad24357709c2f33a
Casey Brant <casey@caseybrant.com>
2017-12-02 09:44:59 -0600

    SHOPPOTRON basic movement

---
+++ Assets/ShoppotronController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppotronController : MonoBehaviour 
+{
+    public float rotation_speed;
+    public float movement_speed;
+
+	void Start () 
+    {
+        
+    }
+	
+	void Update () 
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 rotation = new Vector3(0, 0, -h) * rotation_speed;
+        Vector3 movement = new Vector3(-v, 0, 0) * movement_speed;
+        transform.Rotate(rotation);
+        transform.Translate(movement);
+	}
+}
