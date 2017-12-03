f6c20a4e38d4bf9b31bf1ef46e8b28c774532231
Casey Brant <casey@caseybrant.com>
2017-12-03 16:55:40 -0600

    Player sprite and animation!

--- Assets/Scripts/ShoppotronController.cs
+++ Assets/Scripts/ShoppotronController.cs
@@ -7,9 +7,12 @@ public class ShoppotronController : MonoBehaviour
     public float rotation_speed;
     public float movement_speed;
 
+    private Animator anim;
+
 	void Start () 
     {
-        
+        anim = gameObject.GetComponent<Animator>();
+        anim.speed = 0;
     }
 	
 	void FixedUpdate () 
@@ -17,8 +20,14 @@ public class ShoppotronController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 rotation = new Vector3(0, 0, -h) * rotation_speed;
-        Vector3 movement = new Vector3(-v, 0, 0) * movement_speed;
+        Vector3 movement = new Vector3(0, v, 0) * movement_speed;
         transform.Rotate(rotation);
         transform.Translate(movement);
+
+        if (Mathf.Abs(v) > 0) {
+            anim.speed = 1;
+        } else {
+            anim.speed = 0;
+        }
 	}
 }
