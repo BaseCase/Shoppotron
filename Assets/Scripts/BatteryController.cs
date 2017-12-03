639f6e0921400216072526e1d36cd8bfac2a0485
Casey Brant <casey@caseybrant.com>
2017-12-02 18:28:46 -0600

    Game Over when battery drains

--- Assets/Scripts/BatteryController.cs
+++ Assets/Scripts/BatteryController.cs
@@ -22,12 +22,14 @@ public class BatteryController : MonoBehaviour
         float elapsed = Time.time - start_time;
         float time_left = Mathf.Clamp(seconds_to_live - elapsed, 0f, seconds_to_live);
         float percent_left = time_left / seconds_to_live;
+
         Vector3 new_scale = new Vector3(percent_left, 1f, 1f);
         transform.localScale = new_scale;
         transform.localPosition = new Vector3(-(1f - percent_left) / 2f, 0f, 0f);
 
         if (percent_left <= 0f) {
             game_controller.GetComponent<GameController>().lose_to_battery_drain();
+            Destroy(gameObject);
         }
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour 
+public class GameController : MonoBehaviour
 {
-	void Start () 
+    public GameObject battery_drain_overlay;
+
+    private float time_scale;
+    private bool waiting_for_continue;
+
+    void Start()
     {
-		
-	}
-	
-	void Update () 
+        time_scale = Time.timeScale;
+        waiting_for_continue = false;
+    }
+
+    void Update()
     {
-		
-	}
+        if (waiting_for_continue) {
+            if (Input.GetKeyDown("return")) {
+                Debug.Log("restarting");
+                Time.timeScale = time_scale;
+                Scene loadedLevel = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(loadedLevel.buildIndex);
+            }
+        }
+    }
 
     public void lose_to_battery_drain() 
     {
-        Debug.Log("battery drained!");
+        Time.timeScale = 0f;
+        waiting_for_continue = true;
+        battery_drain_overlay.SetActive(true);
     }
 }
