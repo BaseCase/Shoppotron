1f5608672172de2c5533bc224cddf5694d211983
Casey Brant <casey@caseybrant.com>
2017-12-03 21:22:14 -0600

    Shopping cart!

--- Assets/Scripts/ShoppingCartController.cs
+++ Assets/Scripts/ShoppingCartController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ShoppingCartController : MonoBehaviour 
 {
     public int item_goal;
+    public GameObject item_container;
 
     private int items_captured;
+    private Vector3 first_item_position;
 
 	void Start () 
     {
         items_captured = 0;
+        first_item_position = item_container.transform.position;
 	}
 
     void Update () 
@@ -22,13 +25,11 @@ public class ShoppingCartController : MonoBehaviour
     {
         items_captured++;
         // TODO: only works for one item
-        item.transform.position = transform.position;
+        item.transform.position = first_item_position;
     }
 
     public bool is_full()
     {
-        // TODO: make this generic instead of hard-coded;
-        Debug.Log(items_captured);
         return items_captured >= item_goal;
     }
 }
