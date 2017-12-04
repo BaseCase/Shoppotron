65dc4438252c4f45f780158f57a0c21a6daf84a3
Casey Brant <casey@caseybrant.com>
2017-12-04 09:55:13 -0600

    motor sound effect

--- Assets/Scripts/ShoppotronController.cs
+++ Assets/Scripts/ShoppotronController.cs
@@ -8,11 +8,15 @@ public class ShoppotronController : MonoBehaviour
     public float movement_speed;
 
     private Animator anim;
+    private AudioSource audio;
+    private bool audio_is_playing;
 
 	void Start () 
     {
         anim = gameObject.GetComponent<Animator>();
         anim.speed = 0;
+        audio = gameObject.GetComponent<AudioSource>();
+        audio_is_playing = false;
     }
 	
 	void FixedUpdate () 
@@ -24,5 +28,23 @@ public class ShoppotronController : MonoBehaviour
         transform.Rotate(rotation);
         transform.Translate(movement);
         anim.speed = Mathf.Abs(v);
+
+        { // audio
+            if (Mathf.Abs(v) > 0) {
+                if (audio_is_playing) {
+                    // don't start what's already playing
+                } else {
+                    audio.Play();
+                    audio_is_playing = true;
+                }
+            } else {
+                if (audio_is_playing) {
+                    audio.Stop();
+                    audio_is_playing = false;
+                } else {
+                    // don't stop it again
+                }
+            }
+        }
 	}
 }
